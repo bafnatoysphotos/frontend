595f5759123911118e3cd79d8a348fbff2db8ac4
rishikesh <bafnatoysphotos@gmail.com>
2025-08-20 21:53:05 +0530

    Added vercel.json and updated package.json for Vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8082,   // 👈 ab frontend yahi chalega
+  },
   build: {
-    outDir: 'dist'
-  }
-})
+    outDir: "dist",
+  },
+});
